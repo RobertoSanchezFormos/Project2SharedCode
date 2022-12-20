8e5919f3823fe4c18d7b106c5ee1b534be1b54e0
RobertoSanchez <roberto.sanchez@formos.com>
2022-12-20 11:18:14 -0500

    Job: remove shared Code to be unique

--- src/SharedCode/app/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# To include the project path in the Operating System path:
-print(">>>>>\tAdding project path...")
-script_path = os.path.dirname(os.path.abspath(__file__))
-project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_path)
-
